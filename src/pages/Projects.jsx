7585779b62d1bdc7651af738eb4c701a257a30c0
juanbep <jbecapillimue@gmail.com>
2024-08-28 13:43:55 -0500

    Projects versión 1.3

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import { projects } from "../constants";
 import { Footer } from "../components";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaStar } from "react-icons/fa";
 import ParticlesComponent from "../hooks/particles";
 import { card } from "../assets/images";
 
@@ -40,10 +39,28 @@ const Projects = () => {
               <p className="mt-2 pb-2 text-gray-500 dark:text-neutral-400">
                 {project.description}
               </p>
-              <Link to="/projects" className="button sm:w-1/4 max-sm:w-1/3">
-                GitHub
-                <FaGithub alt="arrow" className="icon object-contain" />
-              </Link>
+              <div className="container-button sm:w-1/4 max-sm:w-1/3">
+                <a
+                  className="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2"
+                  href="#"
+                >
+                  <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
+                  <div className="flex items-center">
+                    <FaGithub alt="github" className="w-4 h-4 fill-current" />
+                    <span className="ml-1 text-white">GitHub</span>
+                  </div>
+                  <div className="ml-2 flex items-center gap-1 text-sm md:flex">
+                    <FaStar
+                      alt="star"
+                      aria-hidden="true"
+                      className="w-4 h-4 text-gray-500 transition-all duration-300 group-hover:text-yellow-300"
+                    />
+                    <span className="inline-block tabular-nums tracking-wider font-display font-medium text-white">
+                      6
+                    </span>
+                  </div>
+                </a>
+              </div>
             </div>
           ))}
         </div>
